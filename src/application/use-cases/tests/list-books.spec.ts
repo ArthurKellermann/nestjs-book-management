50edc1c23929171d5ddf37e18a45307e769a6d2b
Arthur Kellermann <arthur.kellermann956@gmail.com>
2023-08-23 11:05:39 -0300

    test: add unit test to list all the books

---
+++ src/application/use-cases/tests/list-books.spec.ts
@@ -0,0 +1,28 @@
+import { InMemoryBooksRepository } from '@test/repositories/in-memory-books-repository';
+import { ListBooks } from '../list-books';
+import { CreateBook } from '../create-book';
+
+describe('List books', () => {
+  let bookRepository: InMemoryBooksRepository;
+  let listBooks: ListBooks;
+  let createBook: CreateBook;
+
+  beforeEach(() => {
+    bookRepository = new InMemoryBooksRepository();
+    listBooks = new ListBooks(bookRepository);
+    createBook = new CreateBook(bookRepository);
+  });
+
+  it('should be able to list all the books', async () => {
+    const { book } = await createBook.execute({
+      title: 'Harry Potter',
+      description: 'Great book!',
+      bar_code: 'example-bar-code',
+    });
+
+    const { books } = await listBooks.execute();
+
+    expect(books).toHaveLength(1);
+    expect(books[0].id).toEqual(book.id);
+  });
+});
